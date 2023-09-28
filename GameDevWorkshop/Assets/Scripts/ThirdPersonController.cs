f952fe722926d5a05202c80a61fc0076d1aaa344
js369620 <js369620@ohio.edu>
2023-09-28 10:48:19 -0400

    Continued 3PC
    Added movement and jumping; discovered flying bug

--- GameDevWorkshop/Assets/Scripts/ThirdPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/ThirdPersonController.cs
@@ -28,9 +28,11 @@ public class ThirdPersonController : MonoBehaviour
     public Transform orientation;
     public float moveForce; //force applied to player 
     public float rotationSpeed; //speed of model's rotation
-    private Vector3 direction; 
+    private Vector3 direction;
+    public float jumpForce = 500f; //UHHHH 
 
-
+    //raycast vars
+    private float ray_length = 1.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -41,23 +43,35 @@ public class ThirdPersonController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //called every frame, can be a variable amount
     {
-        
+        RotatePlayer();
+
+        Debug.DrawRay(transform.position, Vector3.down * ray_length, Color.blue);
 
 
+    }
+
+    private void FixedUpdate() //specific frames are assigned?
+    {
+        MovePlayer();
     }
 
     public void GetMovementInput(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-
+        Debug.Log(moveInput);
 
     }
 
     public void GetJumpInput(InputAction.CallbackContext context)
     {
-        
+        //when button/key is pressed, do it
+        if (context.phase == InputActionPhase.Started)
+        {
+            Jump();
+            //"Don't be John Bowditch" - John Bowditch
+        }
     }
 
     public void RotatePlayer()
@@ -72,6 +86,41 @@ public class ThirdPersonController : MonoBehaviour
         direction = orientation.right * moveInput.x + orientation.forward * moveInput.y;
         direction = direction.normalized;
 
-        //pick up here
+        //a normal is the direction a UV faces, fun fact
+
+        //keyboard input
+        if (moveInput != Vector2.zero)
+        {
+            //this creates a new rotation that we want the playerModel to look at
+            Quaternion new_rotation = Quaternion.LookRotation(direction, Vector3.up);
+            
+            //the typing of many keys sounds like rain
+
+            //calculated rotation, now we want the playerModel to move towards that rotation
+            playerModel.rotation = Quaternion.Slerp(playerModel.rotation, new_rotation, rotationSpeed * Time.deltaTime);
+                //Slerp is for rotation, Lerp is for 
+                //Slurp is for credit card fraud
+        }
+    }
+
+    public void MovePlayer()
+    {
+        rigidbody.AddForce(direction * moveForce, ForceMode.Force);
+
+
+    }
+
+    public void Jump()
+    {
+        if (IsOnGround())
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        return Physics.Raycast(ray, ray_length);
     }
 }
