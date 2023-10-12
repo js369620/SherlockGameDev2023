4811ff2cfc7f5da9c3fa8518692e3e627d4494aa
js369620 <js369620@ohio.edu>
2023-10-12 10:49:08 -0400

    flashlight sfx
    added SFX to the flashlight; also rave light

--- GameDevWorkshop/Assets/Scripts/Flashlight.cs
+++ GameDevWorkshop/Assets/Scripts/Flashlight.cs
@@ -11,6 +11,7 @@ using UnityEngine.InputSystem;
 public class Flashlight : MonoBehaviour
 {
     public Light light;
+    public AudioSource audioSource; 
 
 
     // Start is called before the first frame update
@@ -22,11 +23,28 @@ public class Flashlight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        float r = Random.Range(0.0f, 1.0f);
+        float g = Random.Range(0.0f, 1.0f);
+        float b = Random.Range(0.0f, 1.0f);
+        light.color = new Color(r, g, b);
+        */
     }
 
     public void OnClick(InputAction.CallbackContext context)
     {
-        light.enabled = !light.enabled; //this is simpler than using an if statement
+        if(context.phase == InputActionPhase.Started)
+        {
+            light.enabled = !light.enabled; //this is simpler than using an if statement
+            audioSource.pitch = Random.Range(0.5f, 1.5f);
+            audioSource.Play();
+            /*
+            float r = Random.Range(0.0f, 1.0f);
+            float g = Random.Range(0.0f, 1.0f);
+            float b = Random.Range(0.0f, 1.0f);
+            light.color = new Color(r, g, b);
+            */
+        }
+        
     }
 }
